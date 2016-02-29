dc2f60016f917b3a1de4f6c73288eb0d10674e6b
denkhaus <peristaltic@gmx.net>
2016-02-29 15:55:56 +0100

    initial commit

---
+++ client.go
@@ -0,0 +1,152 @@
+package splunk
+
+import (
+	"bytes"
+	"encoding/json"
+	"net"
+	"time"
+
+	"github.com/juju/errors"
+)
+
+type SplunkClient struct {
+	address    *net.TCPAddr
+	connection *net.TCPConn
+	queue      chan SplunkMessage
+	done       chan bool
+}
+
+type SplunkMessage struct {
+	Time        time.Time              `json:"time"`
+	Measurement string                 `json:"mesurement"`
+	Fields      map[string]interface{} `json:"fields"`
+}
+
+func NewSplunkClient(addrStr string) (*SplunkClient, error) {
+	if len(addrStr) == 0 {
+		return nil, errors.New("address missing")
+	}
+
+	address, err := net.ResolveTCPAddr("tcp", addrStr)
+	if err != nil {
+		return nil, errors.Annotate(err, "resolve address")
+	}
+
+	queue := make(chan SplunkMessage, 1024)
+	done := make(chan bool, 1)
+
+	client := &SplunkClient{
+		address: address,
+		queue:   queue,
+		done:    done,
+	}
+
+	if err = client.connect(); err != nil {
+		return nil, errors.Annotate(err, "connect")
+	}
+
+	go client.writer()
+	return client, nil
+}
+
+func (p *SplunkClient) connect() error {
+	connection, err := net.DialTCP("tcp", nil, p.address)
+	if err != nil {
+		return errors.Annotate(err, "dialtcp")
+	}
+
+	if err = connection.SetKeepAlive(true); err != nil {
+		return errors.Annotate(err, "set keep alive")
+	}
+
+	p.connection = connection
+	return nil
+}
+
+func (p *SplunkClient) disconnect() error {
+	if p.connection == nil {
+		return nil
+	}
+
+	return p.connection.Close()
+}
+
+func (p *SplunkClient) reconnectLoop() {
+	p.disconnect()
+
+	var err error
+
+	for {
+		select {
+		case <-p.done:
+			break
+		default:
+		}
+
+		err = p.connect()
+		if err == nil {
+			break
+		}
+
+		logger.Errorf("reconnect failed: %s", err)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func (p *SplunkClient) writeData(b []byte) {
+	for {
+		bytesWritten, err := p.connection.Write(b)
+		if err != nil {
+			logger.Errorf("failed to write message: %s", err)
+			p.reconnectLoop()
+			return
+		}
+
+		b = b[bytesWritten:]
+		if len(b) == 0 {
+			break
+		}
+	}
+}
+
+func (p *SplunkClient) writer() {
+	for msg := range p.queue {
+		buf, err := p.buildMessage(msg)
+		if err != nil {
+			logger.Error(errors.Annotate(err, "build message"))
+			return
+		}
+		p.writeData(buf.Bytes())
+	}
+}
+
+func (p *SplunkClient) buildMessage(m SplunkMessage) (*bytes.Buffer, error) {
+	data, err := json.Marshal(&m)
+	if err != nil {
+		return nil, errors.Annotate(err, "marshal")
+	}
+
+	buf := bytes.NewBuffer(data)
+	buf.WriteString("\n")
+	return buf, nil
+
+}
+
+func (p *SplunkClient) Stream(stream chan SplunkMessage) {
+	for message := range stream {
+		select {
+		case p.queue <- message:
+		default:
+			logger.Warning("channel is full. dropping messages")
+			continue
+		}
+	}
+
+	p.Close()
+}
+
+func (p *SplunkClient) Close() {
+	close(p.queue)
+	p.disconnect()
+	p.done <- true
+}
---
+++ splunk.go
@@ -0,0 +1,138 @@
+package splunk
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/rcrowley/go-metrics"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	logger = logrus.New().WithField("pkg", "go-metrics-splunk")
+)
+
+type splunkForwarder struct {
+	reg      metrics.Registry
+	ch       chan SplunkMessage
+	interval time.Duration
+	client   *SplunkClient
+}
+
+// Splunk starts a Splunk metrics forwarder from the given registry at each d interval.
+func Splunk(r metrics.Registry, d time.Duration, splunkAddr string) {
+	rep := &splunkForwarder{
+		ch:       make(chan SplunkMessage),
+		reg:      r,
+		interval: d,
+	}
+
+	c, err := NewSplunkClient(splunkAddr)
+	if err != nil {
+		logger.Errorf("unable to create client: %s", err)
+		return
+	}
+	rep.client = c
+	rep.run()
+}
+
+func (r *splunkForwarder) run() {
+	sendTicker := time.Tick(r.interval)
+	r.client.Stream(r.ch)
+
+	for {
+		select {
+		case <-sendTicker:
+			r.send()
+		}
+	}
+}
+
+func (r *splunkForwarder) send() {
+	r.reg.Each(func(name string, i interface{}) {
+		now := time.Now()
+
+		switch m := i.(type) {
+		case metrics.Counter:
+			r.ch <- SplunkMessage{
+				Measurement: fmt.Sprintf("%s.count", name),
+				Fields: map[string]interface{}{
+					"value": m.Count(),
+				},
+				Time: now,
+			}
+		case metrics.Gauge:
+			r.ch <- SplunkMessage{
+				Measurement: fmt.Sprintf("%s.gauge", name),
+				Fields: map[string]interface{}{
+					"value": m.Value(),
+				},
+				Time: now,
+			}
+		case metrics.GaugeFloat64:
+			r.ch <- SplunkMessage{
+				Measurement: fmt.Sprintf("%s.gauge", name),
+				Fields: map[string]interface{}{
+					"value": m.Value(),
+				},
+				Time: now,
+			}
+		case metrics.Histogram:
+			ps := m.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999, 0.9999})
+			r.ch <- SplunkMessage{
+				Measurement: fmt.Sprintf("%s.histogram", name),
+				Fields: map[string]interface{}{
+					"count":    m.Count(),
+					"max":      m.Max(),
+					"mean":     m.Mean(),
+					"min":      m.Min(),
+					"stddev":   m.StdDev(),
+					"variance": m.Variance(),
+					"p50":      ps[0],
+					"p75":      ps[1],
+					"p95":      ps[2],
+					"p99":      ps[3],
+					"p999":     ps[4],
+					"p9999":    ps[5],
+				},
+				Time: now,
+			}
+		case metrics.Meter:
+			r.ch <- SplunkMessage{
+				Measurement: fmt.Sprintf("%s.meter", name),
+				Fields: map[string]interface{}{
+					"count": m.Count(),
+					"m1":    m.Rate1(),
+					"m5":    m.Rate5(),
+					"m15":   m.Rate15(),
+					"mean":  m.RateMean(),
+				},
+				Time: now,
+			}
+		case metrics.Timer:
+			ps := m.Percentiles([]float64{0.5, 0.75, 0.95, 0.99, 0.999, 0.9999})
+			r.ch <- SplunkMessage{
+				Measurement: fmt.Sprintf("%s.timer", name),
+				Fields: map[string]interface{}{
+					"count":    m.Count(),
+					"max":      m.Max(),
+					"mean":     m.Mean(),
+					"min":      m.Min(),
+					"stddev":   m.StdDev(),
+					"variance": m.Variance(),
+					"p50":      ps[0],
+					"p75":      ps[1],
+					"p95":      ps[2],
+					"p99":      ps[3],
+					"p999":     ps[4],
+					"p9999":    ps[5],
+					"m1":       m.Rate1(),
+					"m5":       m.Rate5(),
+					"m15":      m.Rate15(),
+					"meanrate": m.RateMean(),
+				},
+				Time: now,
+			}
+		}
+	})
+}
